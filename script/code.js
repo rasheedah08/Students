3421093d67bf0ce7e33c6c54b791e2c984f9dfdd
rasheedah08 <rasheedahanhuizen@gmail.com>
2022-07-04 13:06:33 +0200

    first commit

---
+++ script/code.js
@@ -0,0 +1,9 @@
+const student = Vue.createApp({
+    data: function() {
+        return {
+            Name: 'Rasheedah' ,
+            Surname: 'Anhuizen'
+        }
+    }
+});
+student.mount('#details')
